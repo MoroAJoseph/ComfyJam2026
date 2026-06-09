1c561a6e8493f2f7d5e5cd06a2af0b3a11f27d30
Joseph Moro <moroajoseph@gmail.com>
2026-06-09 10:22:01 -0400

    interaction ring

---
+++ game/ui/hud/hud.gd
@@ -0,0 +1,8 @@
+class_name HUD
+extends Control
+
+@export var dialogue_resource: DialogueResource
+@export var dialogue_start: String = "start"
+
+func action() -> void:
+	DialogueManager.show_example_dialogue_balloon(dialogue_resource, dialogue_start)
